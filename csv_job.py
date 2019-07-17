ffac56bbb1867b0d7a81c25fbe13b5f514639151
Allencs <allenchensheng@163.com>
2019-07-17 14:42:48 +0800

    modify comments

--- csv_job.py
+++ csv_job.py
@@ -12,6 +12,7 @@ from zip import ZIP
 
 
 class CSVJob(object):
+
     _csv_files = set()
     """待处理csv文件"""
 
@@ -42,12 +43,14 @@ class CSVJob(object):
         self.start_time = time.time()
 
         self.load_resource_csv()
-
         self.load_csv_model()
 
     @ErrorInfo
     def load_resource_csv(self):
-        """"""
+        """
+        加载资源文件，读取ASC_Code，partNo
+        :return: None
+        """
         self.logger.info("load resource csv")
         resource_files = os.listdir(self.resource_csv)
         for file in resource_files:
@@ -59,13 +62,18 @@ class CSVJob(object):
                             self._asc_codes.append(row[0])
 
             elif "partno" in file:
-                with open(os.path.join(self.resource_csv, file), 'r', newline='') as csvfile:
+                with open(os.path.join(self.resource_csv, file), 'r', newline='', encoding='utf-8') as csvfile:
                     csv_reader = csv.reader(csvfile, dialect='excel')
                     for row in csv_reader:
                         if csv_reader.line_num > 1:
                             self._partNos.append(row[0])
 
+    @ErrorInfo
     def load_csv_model(self):
+        """
+        加载模板文件
+        :return: None
+        """
         self.csv_model_name = os.listdir(self.csv_model_path)[0]
         filepath = os.path.join(self.csv_model_path, self.csv_model_name)
         self.logger.info("load csv model")
@@ -76,6 +84,11 @@ class CSVJob(object):
 
     @ErrorInfo
     def generate_new_csv(self):
+        """
+        将模板数据的ASC_Code和partNo使用资源数据进行替换（partNo从资源中随机抽取），
+        生成以ASC——Code为前缀的CSV文件
+        :return: None
+        """
         print("------partNo个数：{}".format(len(self._partNos)))
         print("------asc_code: {}".format(len(self._asc_codes)))
         if not os.path.exists(self.new_file_dir):
@@ -91,14 +104,14 @@ class CSVJob(object):
                 csv_datas = self._csv_datas.copy()
                 csv_name = asc_code + self.csv_model_name
 
-                with open(os.path.join(self.new_file_dir, csv_name), 'w+', newline='') as csvfile:
+                with open(os.path.join(self.new_file_dir, csv_name), 'w+', newline='', encoding='utf-8') as csvfile:
                     writer = csv.writer(csvfile)
                     lines = 0
                     for count, row in enumerate(csv_datas):
                         if count == 1:
                             row[0] = asc_code
                         elif count > 2:
-                            row[0] = random.choice(self._partNos)
+                            row[0] = self._partNos[count]
                         if lines >= int(self.partNos_count) + 3:
                             break
                         writer.writerow(row)
@@ -115,14 +128,14 @@ class CSVJob(object):
                 if asc_count >= int(self.asc_codes_count):
                     break
 
-                with open(os.path.join(self.new_file_dir, csv_name), 'r', newline='') as csvfile:
+                with open(os.path.join(self.new_file_dir, csv_name), 'w+', newline='', encoding='utf-8') as csvfile:
                     writer = csv.writer(csvfile)
                     lines = 0
                     for count, row in enumerate(self._csv_datas):
                         if count == 1:
                             row[0] = asc_code
                         elif count > 2:
-                            row[0] = random.choice(self._partNos)
+                            row[0] = self._partNos[count]
                         if lines >= int(self.partNos_count) + 3:
                             break
                         writer.writerow(row)
@@ -140,6 +153,9 @@ class CSVJob(object):
     """
 
     def add_zipfiles(self):
+        """
+        添加待压缩CSV文件
+        """
         files = os.listdir(self.new_file_dir)
         for file in files:
             full_filepath = os.path.join(self.new_file_dir, file)
@@ -147,6 +163,11 @@ class CSVJob(object):
                 self._zip_files.add(full_filepath)
 
     def zip(self, thread):
+        """
+        压缩文件，如果thread线程未结束，等待1s继续查找添加CSV文件
+        :param thread: CSV文件处理线程
+        :return: None
+        """
         zip = ZIP()
         self.logger.info("zip thread start")
         while True:
@@ -166,6 +187,7 @@ class CSVJob(object):
         self.logger.info("all tasks done")
         time_used = int((time.time() - self.start_time))
         self.logger.info("spend time: {}s".format(time_used))
+        time.sleep(10)
 
     def start(self):
         threads = []
@@ -180,6 +202,7 @@ class CSVJob(object):
 if __name__ == '__main__':
     csv_job = CSVJob()
     csv_job.start()
+
 
 
 
